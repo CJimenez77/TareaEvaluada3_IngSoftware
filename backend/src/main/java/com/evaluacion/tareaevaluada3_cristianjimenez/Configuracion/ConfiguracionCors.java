c3c0ba39f7b4da6c4a4e6526c842a94d121def3e
Cristian <cristian.jimenez2201@alumnos.ubiobio.cl>
2025-11-29 14:52:04 -0300

    configuracion de cors, para poder conectar frontend

---
+++ backend/src/main/java/com/evaluacion/tareaevaluada3_cristianjimenez/Configuracion/ConfiguracionCors.java
@@ -0,0 +1,15 @@
+package com.evaluacion.tareaevaluada3_cristianjimenez.Configuracion;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ConfiguracionCors implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+}
